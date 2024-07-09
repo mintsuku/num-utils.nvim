df46383b623a215d224fe3f30f544c20b01c190a
mintsuku <moxxydev@gmail.com>
2024-07-09 15:55:45 -0400

    conversion back and forth added

--- lua/num-utils/commands.lua
+++ lua/num-utils/commands.lua
@@ -5,55 +5,45 @@ local function get_number_under_cursor()
   local line = vim.api.nvim_get_current_line()
   local col = vim.api.nvim_win_get_cursor(0)[2]
   
-  -- Search backwards
   local start_idx = col
-  while start_idx > 0 and line:sub(start_idx, start_idx):match("%d") do
+  while start_idx > 0 and line:sub(start_idx, start_idx):match("[%dxXoObB]") do
     start_idx = start_idx - 1
   end
   start_idx = start_idx + 1
   
-  -- Search forwards
   local end_idx = col + 1
-  while end_idx <= #line and line:sub(end_idx, end_idx):match("%d") do
+  while end_idx <= #line and line:sub(end_idx, end_idx):match("[%dxXoObB]") do
     end_idx = end_idx + 1
   end
   end_idx = end_idx - 1
   
-  -- Extract the number
   local num_str = line:sub(start_idx, end_idx)
-  return tonumber(num_str), start_idx, end_idx
+  return num_str, start_idx, end_idx
 end
 
 local function replace_number_under_cursor(new_str)
   local line = vim.api.nvim_get_current_line()
-  local num, start_idx, end_idx = get_number_under_cursor()
-  if num then
+  local num_str, start_idx, end_idx = get_number_under_cursor()
+  if num_str then
     local new_line = line:sub(1, start_idx - 1) .. new_str .. line:sub(end_idx + 1)
     vim.api.nvim_set_current_line(new_line)
   end
 end
 
+local function create_conversion_command(name, conversion_func)
+  vim.api.nvim_create_user_command(name, function()
+    local num_str = get_number_under_cursor()
+    if num_str then
+      replace_number_under_cursor(conversion_func(num_str))
+    end
+  end, {})
+end
+
 M.setup = function(opts)
-  vim.api.nvim_create_user_command('NumToHex', function()
-    local num = get_number_under_cursor()
-    if num then
-      replace_number_under_cursor(converter.to_hex(num))
-    end
-  end, {})
-
-  vim.api.nvim_create_user_command('NumToBinary', function()
-    local num = get_number_under_cursor()
-    if num then
-      replace_number_under_cursor(converter.to_binary(num))
-    end
-  end, {})
-
-  vim.api.nvim_create_user_command('NumToOctal', function()
-    local num = get_number_under_cursor()
-    if num then
-      replace_number_under_cursor(converter.to_octal(num))
-    end
-  end, {})
+  create_conversion_command('NumToHex', converter.to_hex)
+  create_conversion_command('NumToBinary', converter.to_binary)
+  create_conversion_command('NumToOctal', converter.to_octal)
+  create_conversion_command('NumToDecimal', converter.to_decimal)
 
   print("All commands created")
 end
--- lua/num-utils/converter.lua
+++ lua/num-utils/converter.lua
@@ -1,12 +1,38 @@
 local M = {}
 
+local function is_hex(str)
+  return str:match("^0x%x+$") ~= nil
+end
+
+local function is_binary(str)
+  return str:match("^0b[01]+$") ~= nil
+end
+
+local function is_octal(str)
+  return str:match("^0o[0-7]+$") ~= nil
+end
+
+M.to_decimal = function(str)
+  if is_hex(str) then
+    return tonumber(str:sub(3), 16)
+  elseif is_binary(str) then
+    return tonumber(str:sub(3), 2)
+  elseif is_octal(str) then
+    return tonumber(str:sub(3), 8)
+  else
+    return tonumber(str)
+  end
+end
+
 M.to_hex = function(num)
-  return string.format("0x%X", num)
+  if type(num) == "string" and is_hex(num) then return num end
+  return string.format("0x%X", M.to_decimal(num))
 end
 
 M.to_binary = function(num)
+  if type(num) == "string" and is_binary(num) then return num end
+  local n = M.to_decimal(num)
   local binary = ""
-  local n = num
   while n > 0 do
     binary = tostring(n % 2) .. binary
     n = math.floor(n / 2)
@@ -15,7 +41,8 @@ M.to_binary = function(num)
 end
 
 M.to_octal = function(num)
-  return string.format("0o%o", num)
+  if type(num) == "string" and is_octal(num) then return num end
+  return string.format("0o%o", M.to_decimal(num))
 end
 
 return M
