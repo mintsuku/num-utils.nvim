a3d1f696abf2a88d6ab28e5f6d9a67990dc9b292
mintsuku <moxxydev@gmail.com>
2024-07-09 15:27:21 -0400

    more changes

--- lua/num-utils/commands.lua
+++ lua/num-utils/commands.lua
@@ -1,19 +1,33 @@
 local converter = require('num-utils.converter')
-
 local M = {}
 
 local function get_number_under_cursor()
   local line = vim.api.nvim_get_current_line()
   local col = vim.api.nvim_win_get_cursor(0)[2]
-  local num_str = line:match("%d+", col + 1)
-  return tonumber(num_str)
+  
+  -- Search backwards
+  local start_idx = col
+  while start_idx > 0 and line:sub(start_idx, start_idx):match("%d") do
+    start_idx = start_idx - 1
+  end
+  start_idx = start_idx + 1
+  
+  -- Search forwards
+  local end_idx = col + 1
+  while end_idx <= #line and line:sub(end_idx, end_idx):match("%d") do
+    end_idx = end_idx + 1
+  end
+  end_idx = end_idx - 1
+  
+  -- Extract the number
+  local num_str = line:sub(start_idx, end_idx)
+  return tonumber(num_str), start_idx, end_idx
 end
 
 local function replace_number_under_cursor(new_str)
   local line = vim.api.nvim_get_current_line()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  local start_idx, end_idx = line:find("%d+", col + 1)
-  if start_idx and end_idx then
+  local num, start_idx, end_idx = get_number_under_cursor()
+  if num then
     local new_line = line:sub(1, start_idx - 1) .. new_str .. line:sub(end_idx + 1)
     vim.api.nvim_set_current_line(new_line)
   end
@@ -41,8 +55,7 @@ M.setup = function(opts)
     end
   end, {})
 
-
-	  print("All commands created")
+  print("All commands created")
 end
 
 return M
